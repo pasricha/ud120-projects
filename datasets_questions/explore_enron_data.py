b0214e6f828d0b3190b1d79b8f58242ff6bd980f
Nivranshu Pasricha <nivranshu.pasricha@insight-centre.org>
2020-01-29 17:30:53 +0000

    Mini Project: Datasets and Questions

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -20,3 +20,47 @@ import pickle
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 
 
+print 'Data points: ', len(enron_data)
+
+print 'Number of features: ', len(enron_data['SKILLING JEFFREY K'])
+
+poi = 0
+for person in enron_data.keys():
+    if enron_data[person]['poi'] == 1:
+        poi += 1
+print 'POIs: ', poi
+
+# Total POIs: 35 (in ../final_project/poi_names.txt)
+
+print 'Stock belonging to James Prentice: ', \
+    enron_data['PRENTICE JAMES']['total_stock_value']
+
+print 'Messages from Wesley Colwell to POIs', \
+    enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+
+print 'Stock options exercised by Jeffery Skilling: ', \
+    enron_data['SKILLING JEFFREY K']['exercised_stock_options']
+
+nan_total_payments = 0
+for person in enron_data.keys():
+    if enron_data[person]['total_payments'] == 'NaN':
+        nan_total_payments += 1
+print 'Percentage of people with "NaN" total payments: ', \
+    float(nan_total_payments) / len(enron_data)
+
+nan_total_payments_poi = 0
+for person in enron_data.keys():
+    if enron_data[person]['poi']:
+        if enron_data[person]['total_payments'] == 'NaN':
+            nan_total_payments += 1
+print 'Percentage of POIs with "NaN" total payments: ', \
+    float(nan_total_payments_poi) / poi
+
+print 'With 10 more POIs having "NaN" total payments,'
+print 'Number of people: ', 10 + len(enron_data)
+print 'Number of people with "NaN" total payments: ', 10 + nan_total_payments
+print 'New Number of POIs', 10 + poi
+print 'Number of POIs with "NaN" total payments', 10 + nan_total_payments_poi
+
+
+
